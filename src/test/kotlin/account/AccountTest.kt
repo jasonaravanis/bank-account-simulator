2e422f8cd25cc1114eb74bb35906861afd61e9c4
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2026-08-10 08:10:27 +1000

    feat: refactor test organisation to use @Nested

--- src/test/kotlin/account/AccountTest.kt
+++ src/test/kotlin/account/AccountTest.kt
@@ -4,24 +4,28 @@ import java.math.BigDecimal
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import org.junit.jupiter.api.Nested
 
 class AccountTest {
-    @Test
-    fun `withdraw reduces the balance`() {
-        val account = Account(BigDecimal(100))
-        account.withdraw(BigDecimal(30))
-        assertEquals(BigDecimal(70), account.getBalance())
-    }
+    @Nested
+    inner class Withdraw {
+        @Test
+        fun `reduces the balance`() {
+            val account = Account(BigDecimal(100))
+            account.withdraw(BigDecimal(30))
+            assertEquals(BigDecimal(70), account.getBalance())
+        }
 
-    @Test
-    fun `withdrawing more than the balance throws AccountException`() {
-        val account = Account(BigDecimal(50))
+        @Test
+        fun `throws AccountException when withdrawing more than the balance`() {
+            val account = Account(BigDecimal(50))
 
-        val exception =
-            assertFailsWith<AccountException> {
-                account.withdraw(BigDecimal(100))
-            }
+            val exception =
+                assertFailsWith<AccountException> {
+                    account.withdraw(BigDecimal(100))
+                }
 
-        assertEquals("Withdrawal amount cannot exceed current account balance!", exception.message)
+            assertEquals("Withdrawal amount cannot exceed current account balance!", exception.message)
+        }
     }
 }
