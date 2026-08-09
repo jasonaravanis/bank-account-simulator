94d2a45d962a729661dc4da49958889108a30803
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2026-08-10 08:03:41 +1000

    feat: second test

--- src/test/kotlin/account/AccountTest.kt
+++ src/test/kotlin/account/AccountTest.kt
@@ -1,8 +1,9 @@
 package account
 
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
 import java.math.BigDecimal
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class AccountTest {
     @Test
@@ -10,5 +11,17 @@ class AccountTest {
         val account = Account(BigDecimal(100))
         account.withdraw(BigDecimal(30))
         assertEquals(BigDecimal(70), account.getBalance())
+    }
+
+    @Test
+    fun `withdrawing more than the balance throws AccountException`() {
+        val account = Account(BigDecimal(50))
+
+        val exception =
+            assertFailsWith<AccountException> {
+                account.withdraw(BigDecimal(100))
+            }
+
+        assertEquals("Withdrawal amount cannot exceed current account balance!", exception.message)
     }
 }
