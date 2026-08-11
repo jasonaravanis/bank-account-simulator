a79d70f5107c4e647b3a5355cd79eba6cc61bb20
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2026-08-11 19:57:56 +1000

    feat: create account (not saved to state)

--- src/main/kotlin/ui/CreateAccount.kt
+++ src/main/kotlin/ui/CreateAccount.kt
@@ -1,5 +1,34 @@
 package ui
 
+import account.Account
+import java.math.BigDecimal
+
 fun createAccount() {
-    println("Create account - not yet implemented")
+    println("Provide a name for your new account.")
+
+    var accountName: String? = null
+    while (accountName == null) {
+        print("Name: ")
+        val accountNameInput = readln()
+        if (accountNameInput.isEmpty()) {
+            println("Invalid. Please try again.")
+        } else {
+            accountName = accountNameInput
+        }
+    }
+
+    println("Your chosen account name is '$accountName'")
+    println("What is the initial balance of your account?")
+    var initialBalance: BigDecimal? = null
+    while (initialBalance == null) {
+        print("Initial balance: ")
+        val initialBalanceInput = readln().toBigDecimalOrNull()
+        when {
+            (initialBalanceInput == null) -> println("Invalid")
+            (initialBalanceInput < BigDecimal.ZERO) -> println("Invalid")
+            else -> initialBalance = initialBalanceInput
+        }
+    }
+
+    val newAccount = Account(accountName, initialBalance)
 }
