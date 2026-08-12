7af4ace9216e46b9968a46e73f21d036cab6f36d
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2026-08-12 19:22:27 +1000

    feat: add Account and AccountData conversion

--- src/main/kotlin/account/AccountData.kt
+++ src/main/kotlin/account/AccountData.kt
@@ -1,9 +1,14 @@
 package account
 
 import kotlinx.serialization.Serializable
+import java.math.BigDecimal
 
 @Serializable
 data class AccountData(
     val name: String,
     val balance: String,
 )
+
+fun AccountData.toAccount(): Account = Account(name = name, balance = BigDecimal(balance))
+
+fun Account.toAccountData(): AccountData = AccountData(name = getName(), balance = getBalance().toPlainString())
--- src/test/kotlin/account/AccountDataTest.kt
+++ src/test/kotlin/account/AccountDataTest.kt
@@ -2,6 +2,7 @@ package account
 
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.math.BigDecimal
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -13,5 +14,11 @@ class AccountDataTest {
         val deserialized = Json.decodeFromString<AccountData>(serialized)
         assertEquals("""{"name":"mock","balance":"123.50"}""", serialized)
         assertEquals(accountData, deserialized)
+    }
+
+    @Test
+    fun `toAccount converts balance string to BigDecimal`() {
+        val account = AccountData("mock", "123.50").toAccount()
+        assertEquals(account.getBalance(), BigDecimal("123.50"))
     }
 }
